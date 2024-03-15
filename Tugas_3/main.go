bf8ed02bc8db8cef8eaf2f0d33b1230190792c56
ruhefm <herupurnama09@gmail.com>
2024-03-15 18:21:43 +0700

    first push, belum pakai css tampilan

---
+++ Tugas_3/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+	"tugas3/routers"
+)
+
+type Status struct {
+	Water int `json:"water"`
+	Wind  int `json:"wind"`
+}
+
+func acakStatus() Status {
+	return Status{
+		Water: rand.Intn(100) + 1, // Dari 1 ke 100
+		Wind:  rand.Intn(100) + 1, // Dari 1 ke 100
+	}
+}
+
+func jsonSimpan(status Status, filePath string) error {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777) //path file, write read only, create if tidak ada, trunc if ada, granted power privileges.
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(map[string]Status{"status": status})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Updated status:", status)
+	return nil
+}
+
+func rutinitas15(filePath string) {
+
+	for {
+		status := acakStatus()
+		err := jsonSimpan(status, filePath)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+		time.Sleep(15 * time.Second)
+	}
+}
+
+var PORT = ":8080"
+
+func main() {
+	filePath := "status.json"
+	go rutinitas15(filePath)
+	routers.StartServer().Run(PORT)
+}
---
+++ Tugas_3/routers/router.go
@@ -0,0 +1,35 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.File("halaman.html")
+	})
+	router.GET("/status.json", func(c *gin.Context) {
+		c.File("status.json")
+	})
+
+	router.Use(Cors())
+
+	return router
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
+	}
+}
