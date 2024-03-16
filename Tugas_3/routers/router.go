9213586a538c52251e327f124face44f289fee30
ruhefm <herupurnama09@gmail.com>
2024-03-17 03:54:52 +0700

    add new logic to change value by operator

--- Tugas_3/routers/router.go
+++ Tugas_3/routers/router.go
@@ -1,8 +1,37 @@
 package routers
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
+
+func jsonSimpan(status Status, filePath string) error {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777) //path file, write read only, create if tidak ada, trunc if ada, granted power privileges.
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(map[string]Status{"status": status})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Updated status:", status)
+	return nil
+}
+
+type Status struct {
+	Water int `json:"water"`
+	Wind  int `json:"wind"`
+}
 
 func StartServer() *gin.Engine {
 	router := gin.Default()
@@ -12,6 +41,28 @@ func StartServer() *gin.Engine {
 	})
 	router.GET("/status.json", func(c *gin.Context) {
 		c.File("status.json")
+	})
+	router.POST("/pump_water_wind", func(c *gin.Context) {
+		filePath := "status.json"
+		waterIn, err := strconv.Atoi(c.PostForm("water"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid value for water"})
+			return
+		}
+
+		windIn, err := strconv.Atoi(c.PostForm("wind"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid value for wind"})
+			return
+		}
+		status := Status{
+			Water: waterIn,
+			Wind:  windIn,
+		}
+		jsonSimpan(status, filePath)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
 	})
 
 	router.Use(Cors())
